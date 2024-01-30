e4df71077ee88c69207bd8740997630dc53ad6dd
NiklausLS <nicolas.leempoels@gmail.com>
2024-01-30 16:57:21 +0100

    finish ?

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 11:41:21 by nileempo          #+#    #+#             */
-/*   Updated: 2024/01/30 16:47:07 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/01/30 16:54:03 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,5 @@ int	main(int argc, char **argv, char **envp)
 		child_process(data, env);
 	else
 		parent_process(data, env);
-	free_cmds(&data);
-	free_tab(env);
-	free_path(&data);
 	return (0);
 }
